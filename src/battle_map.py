382f0fbf4928d112c8ce3b2f2cfbdeb915ae5546
RuBiRoId86 <ruben.sargsyan2014@yandex.ru>
2017-06-06 22:39:01 +0400

    Some job done on battle_map object.

---
+++ src/battle_map.py
@@ -0,0 +1,24 @@
+class BattleMap:
+    "A field with 10x10 cells. Separate instances can be used for positioning of both own and an enemy's ships."
+
+    def __init__(self):
+        field_side_length_in_cells = 10
+        self.map = [[0 for x in range(field_side_length_in_cells)] for y in range(field_side_length_in_cells)]
+
+    def get_cell_value(self, letter, number):
+
+        letter_code = ord(letter)
+        letter_index = letter_code - 97
+
+        number_index = number - 1
+
+        try:
+            return self.map[letter_index][number_index]
+        except IndexError:
+            print ("The coodrinates are incorrect.")
+
+# battle_map = battle_map()
+#
+# value = battle_map.get_cell_value("b", 8)
+#
+# print (value)
---
+++ src/test.py
@@ -0,0 +1,9 @@
+from battle_map import BattleMap
+
+bm = BattleMap()
+
+value = bm.get_cell_value("b", 8)
+
+print (value)
+
+
