2f92258d2a39c06480b70a160d8edad0a71d1900
RuBiRoId86 <ruben.sargsyan2014@yandex.ru>
2017-07-19 17:27:18 +0400

    Player name set ability developed.

--- GUI/main_GUI_wrapper.py
+++ GUI/main_GUI_wrapper.py
@@ -1,12 +1,29 @@
-from GUI.test_gui2 import Ui_Form
-from PyQt5 import QtWidgets
+from GUI.main_gui2 import Ui_Form
+from PyQt5 import QtCore, QtWidgets
 
+class BattleShipGUI(Ui_Form):
+
+    def setupUi(self, Form):
+        Ui_Form.setupUi(self, Form)
+
+        self.Player1_name.clicked.connect(lambda: self.open_name_dialog(self.Player1_name))
+        self.Player2_name.clicked.connect(lambda: self.open_name_dialog(self.Player2_name))
+
+    def retranslateUi(self, Form):
+        Ui_Form.retranslateUi(self, Form)
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "BattleShip"))
+
+    def open_name_dialog(self, buttonName):
+        name, ok = QtWidgets.QInputDialog.getText(None, "Name", "Enter your name...")
+        if ok:
+            buttonName.setText(name)
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_Form()
+    ui = BattleShipGUI()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
