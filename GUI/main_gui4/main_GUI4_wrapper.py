3be783119c5fe419ddf80ff955e729da12438b32
RuBiRoId86 <ruben.sargsyan2014@yandex.ru>
2017-07-26 14:43:13 +0400

    Player name selection routine in development.

--- GUI/main_gui4/main_GUI4_wrapper.py
+++ GUI/main_gui4/main_GUI4_wrapper.py
@@ -31,20 +31,50 @@ class BattleShipGUI(Ui_MainWindow):
     def start_FSM(self):
 
         #States
+
         self.initial_state = QtCore.QState()
         self.initial_state.assignProperty(self.centralwidget, "enabled", False)
+        self.initial_state.entered.connect(lambda: print("initial_state"))
+        self.initial_state.entered.connect(lambda: self.label.setStyleSheet("color : #555555;"))
 
-        self.state2 = QtCore.QState()
-        self.state2.assignProperty(self.centralwidget, "enabled", True)
+        # self.set_player_names = QtCore.QState()
+        # self.set_player_names.entered.connect(lambda: self.label.setStyleSheet("color : #5500FF;"))
+        # self.set_player_names.entered.connect(lambda: print("set_player_names"))
+
+
+        # self.set_player1_name = QtCore.QState(self.set_player_names)
+        self.set_player1_name = QtCore.QState()
+        self.set_player1_name.entered.connect(lambda: print("set_player1_name"))
+        self.set_player1_name.entered.connect(lambda: self.open_name_dialog(self.Player1_name))
+
+
+        # self.set_player_names.setInitialState(self.set_player1_name)
+
+        # self.set_player2_name = QtCore.QFinalState(self.set_player_names)
+        self.set_player2_name = QtCore.QState()
+        self.set_player2_name.entered.connect(lambda: print("set_player2_name"))
+        self.set_player2_name.entered.connect(lambda: self.open_name_dialog(self.Player2_name))
+
+        self.start_playing = QtCore.QState()
+        self.start_playing.entered.connect(lambda: print("start_playing"))
+        self.start_playing.assignProperty(self.centralwidget, "enabled", True)
+        self.start_playing.entered.connect(lambda: self.statusbar.showMessage("Start ship positioning."))
 
         #Transitions
-        self.initial_state.addTransition(self.actionStart.triggered, self.state2)
+        # self.initial_state.addTransition(self.actionStart.triggered, self.set_player_names)
+        self.initial_state.addTransition(self.actionStart.triggered, self.set_player1_name)
+        # self.set_player_names.addTransition(self.set_player_names.entered, self.set_player1_name)
+        self.set_player1_name.addTransition(self.set_player1_name.exited, self.set_player2_name)
+        self.set_player2_name.addTransition(self.set_player2_name.exited, self.start_playing)
 
         #State Machine
         self.fsm = QtCore.QStateMachine()
 
         self.fsm.addState(self.initial_state)
-        self.fsm.addState(self.state2)
+        # self.fsm.addState(self.set_player_names)
+        self.fsm.addState(self.set_player1_name)
+        self.fsm.addState(self.set_player2_name)
+        self.fsm.addState(self.start_playing)
 
         self.fsm.setInitialState(self.initial_state)
 
