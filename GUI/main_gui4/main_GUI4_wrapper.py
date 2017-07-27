9b0b2a1760aad7919f4f78836f18b72771e0dfdb
RuBiRoId86 <ruben.sargsyan2014@yandex.ru>
2017-07-27 18:27:25 +0400

    Before gameplay development.

--- GUI/main_gui4/main_GUI4_wrapper.py
+++ GUI/main_gui4/main_GUI4_wrapper.py
@@ -7,12 +7,13 @@ class BattleShipGUI(Ui_MainWindow):
     def setupUi(self, Window):
         Ui_MainWindow.setupUi(self, Window)
 
-        # self.Player1_name.clicked.connect(lambda: self.open_name_dialog(self.Player1_name))
-        # self.Player2_name.clicked.connect(lambda: self.open_name_dialog(self.Player2_name))
+        self.Player1_name.clicked.connect(lambda: self.open_name_dialog(self.Player1_name))
+        self.Player2_name.clicked.connect(lambda: self.open_name_dialog(self.Player2_name))
 
         # Menu items
         self.actionExit.setShortcut("Alt+F4")
         self.actionExit.triggered.connect(lambda: sys.exit())
+        self.actionAbout_us.triggered.connect(lambda: self.show_help())
 
         self.start_FSM()
 
@@ -21,36 +22,47 @@ class BattleShipGUI(Ui_MainWindow):
         _translate = QtCore.QCoreApplication.translate
         Window.setWindowTitle(_translate("Window", "BattleShip"))
 
+    def open_name_dialog(self, buttonName):
+        self.statusbar.showMessage("Set the new name of " + buttonName.text() + ".")
+        name, ok = QtWidgets.QInputDialog.getText(None, buttonName.text(), "Enter your name...")
+        if (ok and len(name) > 0):
+            print("The name of", buttonName.text(), "is changed to", name)
+            buttonName.setText(name)
+
     def player_names_dialogs(self):
         buttons = (self.Player1_name, self.Player2_name)
-
         for button in buttons:
-            player = button.objectName().replace("_name", "")
-            while (True):
-                name, ok = QtWidgets.QInputDialog.getText(None, player, "Enter your name...")
-                if (ok and len(name) > 0):
-                    button.setText(name)
-                    print("The name of", player, "is set to", name)
-                    break
+            self.open_name_dialog(button)
+
+    def show_help(self):
+        print("Show help.")
+        QtWidgets.QMessageBox.information(None, "About us", """\
+This application is developed by Ruben Sargsyan.
+Contacts:
+Email: ruben86@rambler.ru
+Phone: 095461767""")
+
 
     def start_FSM(self):
 
         #States
-
         self.initial_state = QtCore.QState()
         self.initial_state.assignProperty(self.centralwidget, "enabled", False)
         self.initial_state.entered.connect(lambda: print("initial_state"))
         self.initial_state.entered.connect(lambda: self.label.setStyleSheet("color : #555555;"))
+        self.initial_state.entered.connect(lambda: self.statusbar.showMessage("To start the game press Game->Start."))
 
         self.set_player_names = QtCore.QState()
-        self.set_player_names.entered.connect(lambda: self.label.setStyleSheet("color : #5500FF;"))
         self.set_player_names.entered.connect(lambda: print("set_player_names"))
+        self.set_player_names.entered.connect(lambda: self.label.setStyleSheet("color : #5500FF;"))
         self.set_player_names.entered.connect(lambda: self.player_names_dialogs())
 
         self.start_playing = QtCore.QState()
         self.start_playing.entered.connect(lambda: print("start_playing"))
         self.start_playing.assignProperty(self.centralwidget, "enabled", True)
         self.start_playing.entered.connect(lambda: self.statusbar.showMessage("Start ship positioning."))
+        # self.start_playing.entered.connect(lambda: print("Position of checkbox is", self.gridLayout.getItemPosition(self.gridLayout.indexOf(self.checkBox_3))[0]))
+
 
         #Transitions
         self.initial_state.addTransition(self.actionStart.triggered, self.set_player_names)
@@ -69,7 +81,6 @@ class BattleShipGUI(Ui_MainWindow):
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     Window = QtWidgets.QMainWindow()
     ui = BattleShipGUI()
